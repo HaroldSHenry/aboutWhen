76649f4f23d16aa9b3efc772a461ab3667ca81a9
HaroldSHenry <harold@talerian.com>
2013-07-24 07:22:50 -0700

    Minor tweaks.

--- js/jquery-aboutWhen.js
+++ js/jquery-aboutWhen.js
@@ -14,24 +14,24 @@
  * Uses the same license as jQuery, see:
  * http://docs.jquery.com/License
  */
-( function( $ ) {
-  $.fn.aboutWhen = function( settings ) {
-    return this.each( function( settings ) {
+( function ( $ ) {
+  $.fn.aboutWhen = function ( settings ) {
+    return this.each( function ( settings ) {
       var text = ( ( settings ) &&
                    ( settings.text ) ) ?
                   settings.text : {
-                    Nm: [ "(year)", "(month)", "(day)", "(weekday)" ],
-                    Mo: [ "January", "February", "March",
+                    Nm: ["(year)", "(month)", "(day)", "(weekday)"],
+                    Mo: ["January", "February", "March",
                           "April", "May", "June",
                           "July", "August", "September",
-                          "October", "November", "December" ],
-                    Wd: [ "Sunday", "Monday", "Tuesday", "Wednesday",
-                           "Thursday", "Friday", "Saturday" ],
+                          "October", "November", "December"],
+                    Wd: ["Sunday", "Monday", "Tuesday", "Wednesday",
+                           "Thursday", "Friday", "Saturday"],
                     ns: "(not sure)"
                   },
           wdth = ( ( settings ) &&
                    ( settings.widths ) ) ?
-                  settings.widths : [ "80px", "120px", "70px", "125px" ];
+                  settings.widths : [80, 120, 70, 125],
           minY = ( ( settings ) &&
                    ( settings.minY ) &&
                    ( settings.minY > 1800 ) &&
@@ -41,14 +41,15 @@
                    ( settings.maxY ) &&
                    ( settings.maxY > 0 ) &&
                    ( settings.maxY < 2060 ) ) ?
-                  settings.maxY : new Date( ).getFullYear( ),
+                  settings.maxY : new Date().getFullYear(),
           bId = this.id + "_",
           i = 0,
+          pos = 5,
           sel = null,
           opt = null,
           lbl = null,
-          
-          _bldS = function( sId, wid ) {
+
+          _bldS = function ( sId, wid ) {
             var sel = document.createElement( "select" ),
                 opt = document.createElement( "option" );
             sel.id = sId;
@@ -60,23 +61,23 @@
             opt.setAttribute( "value", -1 );
             opt.innerHTML = "";
             sel.appendChild( opt );
-            $(sel).css( { "width" : wid } );
+            $( sel ).css( { "width": wid + 'px' } );
             return sel;
           },
-          
-          _bldL = function( lId, lNm, pos ) {
+
+          _bldL = function ( lId, lNm, pos ) {
             var lbl = document.createElement( "label" );
             lbl.setAttribute( "id", lId );
             lbl.className = 'hsh-when-L';
             lbl.innerHTML = lNm;
             $( lbl ).css( {
-              'left' : pos.left,
-              'top' : pos.top
+              'left': pos,
+              'top': "2px"
             } );
             return lbl;
           },
-          
-          chg = function ( ) {
+
+          chg = function () {
             var pId = '#' + this.id.substr( 0, this.id.length - 1 ),
                 yS = $( pId + "y" )[0],
                 mS = $( pId + "m" )[0],
@@ -89,10 +90,10 @@
                 md = 31,
                 d = dS.selectedIndex,
                 dt = null;
-                
-            if( m > 0 ) {
-              if( m < 9 ) {
-                if( m == 2 ) {
+
+            if ( m > 0 ) {
+              if ( m < 9 ) {
+                if ( m == 2 ) {
                   md = ( ly ) ? 29 : 28;
                 } else {
                   md = ( m & 1 ) ? 31 : 30;
@@ -101,98 +102,101 @@
                 md = ( m & 1 ) ? 30 : 31;
               }
             }
-            if( d > md ) {
+            if ( d > md ) {
               d = md;
               dS.selectedIndex = md;
             }
-            if( ( y > 0 ) && ( m > 0 ) && ( d > 0 ) ) {
+            if ( ( y > 0 ) && ( m > 0 ) && ( d > 0 ) ) {
               dt = new Date( y, m, d );
-              if( dt ) {
-                $( pId + "w" )[0].selectedIndex = dt.getDay( ) + 1;
-                $( pId + "wL" ).hide( );
+              if ( dt ) {
+                $( pId + "w" )[0].selectedIndex = dt.getDay() + 1;
+                $( pId + "wL" ).hide();
               }
             }
           },
-          
-          hideLabel = function( ) {
+
+          hideLabel = function () {
             this.options[0].innerHTML = text.ns;
-            $('#' + this.id + 'L').hide( );
+            $( '#' + this.id + 'L' ).hide();
           },
-          
-          showLabel = function( ) {
+
+          showLabel = function () {
             this.options[0].innerHTML = "";
-            var $L = $('#' + this.id + 'L');
-            if( this.selectedIndex > 0 )
-              $L.hide( );
+            var $L = $( '#' + this.id + 'L' );
+            if ( this.selectedIndex > 0 )
+              $L.hide();
             else
-              $L.show( );
+              $L.show();
           };
-         
-      if( maxY < minY )
+
+      if ( maxY < minY )
         maxY = minY;
-        
+
       // Create the year <select>
       sel = _bldS( bId + "y", wdth[0] );
-      for( i = maxY; i >= minY; i-- ) {
+      for ( i = maxY; i >= minY; i-- ) {
         opt = document.createElement( "option" );
         opt.setAttribute( "value", i );
         opt.innerHTML = "&#160;" + i;
         sel.appendChild( opt );
       }
       this.appendChild( sel );
-      
+
       // Create the year label
-      lbl = _bldL( bId + "yL", text.Nm[0], $( sel ).position( ) );
+      lbl = _bldL( bId + "yL", text.Nm[0], pos );
       this.appendChild( lbl );
 
       // Create the Month <select>
       sel = _bldS( bId + "m", wdth[1] );
-      for( i = 0; i < 12; i++ ) {
+      for ( i = 0; i < 12; i++ ) {
         opt = document.createElement( "option" );
         opt.setAttribute( "value", i );
         opt.innerHTML = "&#160;" + text.Mo[i];
         sel.appendChild( opt );
       }
       this.appendChild( sel );
-      
+
       // Create the month label
-      lbl = _bldL( bId + "mL", text.Nm[1], $( sel ).position( ) );
+      pos += wdth[0] + 2;
+      lbl = _bldL( bId + "mL", text.Nm[1], pos );
       this.appendChild( lbl );
 
       // Create the Day <select>
       sel = _bldS( bId + "d", wdth[2] );
-      for( i = 1; i < 10; i++ ) {
+      for ( i = 1; i < 10; i++ ) {
         opt = document.createElement( "option" );
         opt.setAttribute( "value", i );
         opt.innerHTML = "&#160;&#160;&#160;&#160;" + i;
         sel.appendChild( opt );
       }
-      for( i = 10; i < 32; i++ ) {
+      for ( i = 10; i < 32; i++ ) {
         opt = document.createElement( "option" );
         opt.setAttribute( "value", i );
         opt.innerHTML = "&#160;&#160;&#160;" + i;
         sel.appendChild( opt );
       }
       this.appendChild( sel );
-      
+
       // Create the day label
-      lbl = _bldL( bId + "dL", text.Nm[2], $( sel ).position( ) );
+      pos += wdth[1] + 2;
+      lbl = _bldL( bId + "dL", text.Nm[2], pos );
       this.appendChild( lbl );
 
       // Create the Day-of-the-week <select>
       sel = _bldS( bId + "w", wdth[3] );
-      for( i = 0; i < 7; i++ ) {
+      for ( i = 0; i < 7; i++ ) {
         opt = document.createElement( "option" );
         opt.setAttribute( "value", i );
         opt.innerHTML = "&#160;" + text.Wd[i];
         sel.appendChild( opt );
       }
-      this.appendChild( sel );   
-      
+      this.appendChild( sel );
+
       // Create the day-of-the-week label
-      lbl = _bldL( bId + "wL", text.Nm[3], $( sel ).position( ) );
+      pos += wdth[2] + 2;
+      lbl = _bldL( bId + "wL", text.Nm[3], pos );
       this.appendChild( lbl );
-    });
+    } );
   };
-})( jQuery );
+} )( jQuery );
 
